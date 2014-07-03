fbd2be3dd246507602479c4675ffbff099de4868
Jeff Taggart <jeff@jetaggart.com>
2014-07-03 05:51:31 -0600

    Added basic database connection

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "active_record"
-require "./lib/database"
+require "./lib/database_connection"
 
 class App < Sinatra::Application
+  def initialize
+    super
+    @database_connection = DatabaseConnection.new(ENV["RACK_ENV"])
+  end
 end
