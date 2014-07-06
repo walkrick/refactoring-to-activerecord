6437d768b24d6942aaa7e184b642ee46c9725cc9
Jeff Taggart <jeff@jetaggart.com>
2014-07-06 15:45:18 -0600

    Added example root

--- app.rb
+++ app.rb
@@ -7,4 +7,10 @@ class App < Sinatra::Application
     super
     @database_connection = DatabaseConnection.new(ENV["RACK_ENV"])
   end
+
+  get "/" do
+    @database_connection.sql("INSERT INTO users (name) VALUES ('hello')")
+
+    @database_connection.sql("SELECT * from users").to_s
+  end
 end
