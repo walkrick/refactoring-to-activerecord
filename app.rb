79f25a997051b54b2aba0e46a58615b584732e2c
Hunter Gillane <hunter.gillane@gmail.com>
2014-07-30 10:04:07 -0600

    add validations to registrations

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require "sinatra"
 require "gschool_database_connection"
+require "rack-flash"
 
 class App < Sinatra::Application
+  enable :sessions
+  use Rack::Flash
+
   def initialize
     super
     @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
@@ -13,5 +17,38 @@ class App < Sinatra::Application
 
   get "/register" do
     erb :register
+  end
+
+  post "/registrations" do
+    if validate_registration_params
+      flash[:notice] = "Thanks for registering"
+      redirect "/"
+    else
+      erb :register
+    end
+  end
+
+  private
+
+  def validate_registration_params
+    if params[:username] != "" && params[:password].length > 3
+      return true
+    end
+
+    error_messages = []
+
+    if params[:username] == ""
+      error_messages.push("Username is required")
+    end
+
+    if params[:password] == ""
+      error_messages.push("Password is required")
+    elsif params[:password].length < 4
+      error_messages.push("Password must be at least 4 characters")
+    end
+
+    flash[:notice] = error_messages.join(", ")
+
+    false
   end
 end
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,6 +6,21 @@ feature "Registration and authentication" do
 
     click_link "Register"
 
-    expect(page).to have_content "Register"
+    click_button "Register"
+
+    expect(page).to have_content "Username is required"
+    expect(page).to have_content "Password is required"
+
+    fill_in "Username", with: "hoonta"
+    fill_in "Password", with: "t0p"
+    click_button "Register"
+
+    expect(page).to have_content "Password must be at least 4 characters"
+
+    fill_in "Username", with: "hoonta"
+    fill_in "Password", with: "t0psecret"
+    click_button "Register"
+
+    expect(page).to have_content "Thanks for registering"
   end
 end
